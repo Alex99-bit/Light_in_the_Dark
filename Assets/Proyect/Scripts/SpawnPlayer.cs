cfebc802280ebb634386bb6603e71b046c1bd68a
Carlos Alejandro <alexco_99@hotmail.com>
2024-04-09 16:44:27 -0600

    +-+-+-+

--- Assets/Proyect/Scripts/SpawnPlayer.cs
+++ Assets/Proyect/Scripts/SpawnPlayer.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class SpawnPlayer : MonoBehaviour
 {
+    public GameObject playerPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Instantiate(playerPrefab,this.gameObject.transform);
     }
 }
