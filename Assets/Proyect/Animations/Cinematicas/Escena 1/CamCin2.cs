fac6ad83ffdab7ad3ecd4f86b3a25fae1809fb9e
Carlos Alejandro <alexco_99@hotmail.com>
2024-04-26 03:35:33 -0600

    +++

--- Assets/Proyect/Animations/Cinematicas/Escena 1/CamCin2.cs
+++ Assets/Proyect/Animations/Cinematicas/Escena 1/CamCin2.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 
 public class CamCin2 : MonoBehaviour
 {
+    [SerializeField]
+    GameObject player, playerNPC;
+
+    private void Start() {
+        player.SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+
     public void FinishScene(){
         LD_GameManager.GameManager.instance.ChangeGameState(GameState.InGame);
+        player.SetActive(true);
+        playerNPC.SetActive(false);
         this.gameObject.SetActive(false);
     }
 }
