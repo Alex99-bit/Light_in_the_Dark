d8dfc6d9ada4e12355c3c182c34f31fde77cb1d9
Carlos Alejandro <alexco_99@hotmail.com>
2024-05-07 01:12:39 -0600

    siuuuuuuuuu gol de messi

--- Assets/Proyect/Scripts/ScriptsLvl1/OpenFencePark.cs
+++ Assets/Proyect/Scripts/ScriptsLvl1/OpenFencePark.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class OpenFencePark : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject fencePark1, fencePark2, fencePark1Open, fencePark2Open;
+    public Animator fenceManor1, fenceManor2;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player")){
+            fencePark1.SetActive(false);
+            fencePark2.SetActive(false);
+            fencePark1Open.SetActive(true);
+            fencePark2Open.SetActive(true);
+
+            fenceManor1.SetTrigger("Close");
+            fenceManor2.SetTrigger("Close");
+        }
     }
 }
