3d8ea9c7ddf86d7d63367d847ad373c9a665fe9b
Carlos Alejandro <alexco_99@hotmail.com>
2024-05-08 06:28:55 -0600

    mas cosas para las cinematicas

--- Assets/Proyect/Scripts/Escenas/StartScene2.cs
+++ Assets/Proyect/Scripts/Escenas/StartScene2.cs
@@ -6,13 +6,15 @@ using LD_GameManager;
 public class StartScene2 : MonoBehaviour
 {
     public GameObject cameraHouseDevil;
+    public GameObject player, playerCM1;
 
     //En lugar de iniciar la escena, pasa a una cinematica
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Player")){
+            player.SetActive(false);
+            playerCM1.SetActive(true);
             cameraHouseDevil.SetActive(true);
         }
     }
-
     
 }
