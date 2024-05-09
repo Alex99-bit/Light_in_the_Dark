e78f59b45de39d19b9232d91db29982a8044dcac
Carlos Alejandro <alexco_99@hotmail.com>
2024-05-08 23:34:17 -0600

    +++

--- Assets/Proyect/Scripts/ScriptsTutorial/CN1Event.cs
+++ Assets/Proyect/Scripts/ScriptsTutorial/CN1Event.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class CN1Event : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator ballAnimator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void IniciarAnimacion2BallLight(){
+        ballAnimator.SetTrigger("Next1");
+    } 
 }
