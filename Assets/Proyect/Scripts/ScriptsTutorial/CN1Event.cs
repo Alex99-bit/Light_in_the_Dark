a8126c6beed02f18727e1ac81c0f96103723b6a8
Carlos Alejandro <alexco_99@hotmail.com>
2024-05-11 20:10:55 -0600

    +++

--- Assets/Proyect/Scripts/ScriptsTutorial/CN1Event.cs
+++ Assets/Proyect/Scripts/ScriptsTutorial/CN1Event.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LD_GameManager;
 
 public class CN1Event : MonoBehaviour
 {
     public Animator ballAnimator;
+
+    private void Start() {
+        GameManager.instance.ChangeGameState(GameState.cinematic);
+    }
 
     public void IniciarAnimacion2BallLight(){
         ballAnimator.SetTrigger("Next1");
