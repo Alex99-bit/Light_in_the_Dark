a439885e2d1533a595af862ba4365dc61fac195d
Carlos Alejandro <alexco_99@hotmail.com>
2024-05-15 14:34:36 -0600

    Update TutorialManager.cs

--- Assets/Proyect/Scripts/ScriptsTutorial/TutorialManager.cs
+++ Assets/Proyect/Scripts/ScriptsTutorial/TutorialManager.cs
@@ -5,5 +5,23 @@ using LD_GameManager;
 
 public class TutorialManager : MonoBehaviour
 {
-    
+    public GameObject panelDragDrop, panelDisparo, panelShield;
+
+    private void Start() {
+        panelDisparo.SetActive(false);
+        panelDragDrop.SetActive(false);
+        panelShield.SetActive(false);
+    }
+
+    public void ShowDragDrop(){
+        panelDragDrop.SetActive(true);
+    }
+
+    public void ShowDisparo(){
+        panelDisparo.SetActive(true);
+    }
+
+    public void ShowShield(){
+        panelShield.SetActive(true);
+    }
 }
