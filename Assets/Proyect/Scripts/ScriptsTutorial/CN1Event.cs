2878f8155b651af55434e41b9968c2610b643c4d
Carlos Alejandro <alexco_99@hotmail.com>
2024-05-15 03:22:22 -0600

    wtf que esta pasandaaaa

--- Assets/Proyect/Scripts/ScriptsTutorial/CN1Event.cs
+++ Assets/Proyect/Scripts/ScriptsTutorial/CN1Event.cs
@@ -6,9 +6,20 @@ using LD_GameManager;
 public class CN1Event : MonoBehaviour
 {
     public Animator ballAnimator;
+    public GameObject cam1CN1, cam2CN1, cam3CN1, cam4CN1;
 
     private void Start() {
         GameManager.instance.ChangeGameState(GameState.cinematic);
+        cam1CN1.SetActive(true);
+        cam2CN1.SetActive(false);
+        cam3CN1.SetActive(false);
+        cam4CN1.SetActive(false);
+    }
+
+    private void Update() {
+        if(GameManager.instance.currentGameState == GameState.InGame){
+            SetOfAllCams();
+        }
     }
 
     public void CN1EventBallLight(){
@@ -18,6 +29,24 @@ public class CN1Event : MonoBehaviour
 
     public void Cam2()
     {
-        
+        cam2CN1.SetActive(true);
+        cam1CN1.SetActive(false);
+    }
+
+    public void Cam3(){
+        cam3CN1.SetActive(true);
+        cam2CN1.SetActive(false);
+    }
+
+    public void Cam4(){
+        cam4CN1.SetActive(true);
+        cam3CN1.SetActive(false);
+    }
+
+    public void SetOfAllCams(){
+        cam1CN1.SetActive(false);
+        cam2CN1.SetActive(false);
+        cam3CN1.SetActive(false);
+        cam4CN1.SetActive(false);
     }
 }
