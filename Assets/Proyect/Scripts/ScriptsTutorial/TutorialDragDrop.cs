67f6a60a13fdd14995f73b207398abf8feb59cb9
Carlos Alejandro <alexco_99@hotmail.com>
2024-05-20 20:56:30 -0600

    Update TutorialDragDrop.cs

--- Assets/Proyect/Scripts/ScriptsTutorial/TutorialDragDrop.cs
+++ Assets/Proyect/Scripts/ScriptsTutorial/TutorialDragDrop.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class TutorialDragDrop : MonoBehaviour
 {
+    // Tiempo que transcurre antes de cerrarse
+    public float timeMax = 2, currentTime;
+    
+    // Panel Drag Drop
+    bool panelDragDrop;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = 0;
+        panelDragDrop = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (panelDragDrop)
+        {
+            currentTime += Time.deltaTime;
+            if(currentTime >= timeMax){
+                currentTime = 0;
+                panelDragDrop = false;
+                TutorialManager.instance.ShowDragDrop(panelDragDrop);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.CompareTag("Player")){
+            panelDragDrop = true;
+            TutorialManager.instance.ShowDragDrop(panelDragDrop);
+        }    
     }
 }
